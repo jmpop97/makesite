c6e8fc8aa1e13231c8b875cab2c9423ea630f235
jmpop <pjm970128@gmail.com>
2023-04-08 15:52:36 +0900

    display datetime

--- user/admin.py
+++ user/admin.py
@@ -3,7 +3,8 @@ from .models import UserModel
 
 class TimeDisplay(admin.ModelAdmin):
     list_display = ('username','email','password','created_at','updated_at')
-
+    fields =('username','email','password','created_at','updated_at')
+    readonly_fields = ('created_at','updated_at')
 
 # Register your models here.
 admin.site.register(UserModel,TimeDisplay) # 이 코드가 나의 UserModel을 Admin에 추가 해 줍니다py
